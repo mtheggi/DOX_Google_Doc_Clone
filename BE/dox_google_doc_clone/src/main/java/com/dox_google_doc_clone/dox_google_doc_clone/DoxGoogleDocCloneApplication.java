47eb7438fc61c71fddd02f7a9f07faa47bad0dcc
Ahmed Yasser Hussein <93765400+AhmedYasser20@users.noreply.github.com>
2024-03-10 22:09:40 +0200

    Backend Environment

---
+++ BE/dox_google_doc_clone/src/main/java/com/dox_google_doc_clone/dox_google_doc_clone/DoxGoogleDocCloneApplication.java
@@ -0,0 +1,13 @@
+package com.dox_google_doc_clone.dox_google_doc_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoxGoogleDocCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoxGoogleDocCloneApplication.class, args);
+	}
+
+}
---
+++ BE/dox_google_doc_clone/src/test/java/com/dox_google_doc_clone/dox_google_doc_clone/DoxGoogleDocCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dox_google_doc_clone.dox_google_doc_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoxGoogleDocCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
