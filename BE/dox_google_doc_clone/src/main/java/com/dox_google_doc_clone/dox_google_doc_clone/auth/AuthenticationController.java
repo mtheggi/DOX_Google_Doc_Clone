4ca6d542415f24d1cb73918c4cdfd082af041525
Ahmed Yasser Hussein <93765400+AhmedYasser20@users.noreply.github.com>
2024-04-06 21:57:57 +0200

    Update AuthenticationController.java

--- BE/dox_google_doc_clone/src/main/java/com/dox_google_doc_clone/dox_google_doc_clone/auth/AuthenticationController.java
+++ BE/dox_google_doc_clone/src/main/java/com/dox_google_doc_clone/dox_google_doc_clone/auth/AuthenticationController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dox_google_doc_clone.dox_google_doc_clone.Repositories.UserRepository;
