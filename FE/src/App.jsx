4ed9081272ec403eca769fcb7615c85520db01ca
Mohamed Heggi <mtheggi@gmail.com>
2024-04-19 15:36:21 +0200

    deploy data base

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -3,22 +3,25 @@ import LogIn from './views/Login'
 import SignUp from './views/Signup'
 import Home from './views/Home'
 import TextEditor from './views/TextEditor'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Quill from './Components/CustomizedQuill'
+import Stomp from 'stompjs';
+import SockJS from 'sockjs-client';
+
 
 function App() {
-  const [isNotFound, setIsNotFound] = useState(false); 
+  const [isNotFound, setIsNotFound] = useState(false);
   return (
     <Router>
       <div className="App h-screen flex flex-col bg-gray-200 overflow-x-hidden">
         <Routes>
           <Route path="/" element={<LogIn />} />
-          <Route path="/signup" element={<SignUp/>} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/home" element={<Home />} />
           <Route path="/texteditor" element={
-              <TextEditor /> 
-             } />
+            <TextEditor />
+          } />
         </Routes>
       </div>
     </Router>
