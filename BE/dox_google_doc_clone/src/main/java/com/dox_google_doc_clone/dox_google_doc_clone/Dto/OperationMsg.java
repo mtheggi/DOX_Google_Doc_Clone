1fa82bb595203efa8413c9a42f4f5ffdc995d56b
Mohamed Heggi <mtheggi@gmail.com>
2024-05-09 00:42:05 +0300

    fixing DTo

--- BE/dox_google_doc_clone/src/main/java/com/dox_google_doc_clone/dox_google_doc_clone/Dto/OperationMsg.java
+++ BE/dox_google_doc_clone/src/main/java/com/dox_google_doc_clone/dox_google_doc_clone/Dto/OperationMsg.java
@@ -16,7 +16,7 @@ public class OperationMsg {
     private String character;
     private String siteId;
     private int counter; // index of the character in the document
-    private float fractionIndex; // index of the character in the document
+    private String fractionIndex; // index of the character in the document
     @Override
     public String toString() {
         return "OperationMsg{" +
