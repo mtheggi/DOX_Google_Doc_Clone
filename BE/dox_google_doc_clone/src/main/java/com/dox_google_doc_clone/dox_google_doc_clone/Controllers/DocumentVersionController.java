4879576e91b76b53069cadc586d9133b6d65dd51
Ahmed Yasser Hussein <93765400+AhmedYasser20@users.noreply.github.com>
2024-05-13 01:03:33 +0300

    fixed the bug in the document version controller

--- BE/dox_google_doc_clone/src/main/java/com/dox_google_doc_clone/dox_google_doc_clone/Controllers/DocumentVersionController.java
+++ BE/dox_google_doc_clone/src/main/java/com/dox_google_doc_clone/dox_google_doc_clone/Controllers/DocumentVersionController.java
@@ -21,8 +21,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @CrossOrigin(origins = "*")
 @RestController
