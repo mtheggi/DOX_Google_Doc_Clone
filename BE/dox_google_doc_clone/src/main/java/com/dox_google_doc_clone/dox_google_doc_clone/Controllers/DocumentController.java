74dfc71f9a6f0f9b85ff97c6fee830bceaef87ff
Ahmed Yasser Hussein <93765400+AhmedYasser20@users.noreply.github.com>
2024-05-15 01:07:26 +0300

    fixed again same debug xD

--- BE/dox_google_doc_clone/src/main/java/com/dox_google_doc_clone/dox_google_doc_clone/Controllers/DocumentController.java
+++ BE/dox_google_doc_clone/src/main/java/com/dox_google_doc_clone/dox_google_doc_clone/Controllers/DocumentController.java
@@ -125,11 +125,9 @@ public class DocumentController {
         }
 
         DocumentModel temp = documentService.getDocumentModel(doc_id);
-        System.err.println(temp);
+
         if (managerOfCRDTS.checkMap(doc_id)) {
-            System.err.println("HERE2");
-            System.err.println(managerOfCRDTS.SavedInDB(doc_id));
-            System.err.println("HERE3");
+
             temp.setContent(managerOfCRDTS.SavedInDB(doc_id));
         } else {
             managerOfCRDTS.addCRDTS(doc_id, temp.getContent());
